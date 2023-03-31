d50b3da2ab54a72c641b3a63fc4c9328f7171d3d
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-31 18:55:50 +0530

    contollerssetup

---
+++ backend/controllers/resController.js
@@ -0,0 +1,53 @@
+const Resource = require("../models/resModel");
+const mongoose = require("mongoose");
+
+// get all resources
+const getResources = async (req, res) => {
+  const rs = await Resource.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(rs);
+};
+
+// get a single resource
+const getResource = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such resource" });
+  }
+
+  const rs = await Resource.findById(id);
+
+  if (!rs) {
+    return res.status(404).json({ error: "No such resource" });
+  }
+
+  res.status(200).json(rs);
+};
+
+// create a new resource
+const createResource = async (req, res) => {
+  const { title, links, videos } = req.body;
+
+  // add to the database
+  try {
+    const rs = await Resource.create({ title, links, videos });
+    res.status(200).json(rs);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a resource
+const deleteResource = async (req, res) => {};
+
+// update a resource
+const updateResource = async (req, res) => {};
+
+module.exports = {
+  getResources,
+  getResource,
+  createResource,
+  deleteResource,
+  updateResource,
+};
--- backend/routes/resource.js
+++ backend/routes/resource.js
@@ -1,38 +1,27 @@
 const express = require("express");
-const Resource = require("../models/resModel");
+const {
+  getResources,
+  getResource,
+  createResource,
+  deleteResource,
+  updateResource,
+} = require("../controllers/resController");
 
 const router = express.Router();
 
 // GET all resource
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all resource" });
-});
+router.get("/", getResources);
 
 // GET a single resource
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single resource" });
-});
+router.get("/:id", getResource);
 
 // POST a new resource
-router.post("/", async (req, res) => {
-  const { title, links, videos } = req.body;
-
-  try {
-    const rS = await Resource.create({ title, links, videos });
-    res.status(200).json(rS);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createResource);
 
 // DELETE a resource
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "DELETE a resource" });
-});
+router.delete("/:id", deleteResource);
 
 // UPDATE a resource
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "UPDATE a resource" });
-});
+router.patch("/:id", updateResource);
 
 module.exports = router;

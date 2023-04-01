2142ec558e8c682f064b350ddfedd629d3286cef
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-04-01 08:40:21 +0530

    checktosimplefetch

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
+// components
 const Home = () => {
+  const [resource, setResource] = useState(null);
+
+  useEffect(() => {
+    const fetchResource = async () => {
+      const response = await fetch("/api/resources");
+      const json = await response.json();
+
+      if (response.ok) {
+        setResource(json);
+      }
+    };
+
+    fetchResource();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="resource">
+        {resource && resource.map((res) => <p key={res._id}>{res.title}</p>)}
+      </div>
     </div>
   );
 };
